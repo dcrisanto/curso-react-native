5b3bb4c14a3040f449d2d4d314d6050916c994c9
dcrisanto <dorelly.crisanto@gmail.com>
2022-07-08 23:27:10 -0500

    propTypes

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Text} from "react-native";
+import PropTypes from "prop-types";
 
 export default function Greeting(props) {
     // const {firstname="Ron", lastname="Con pasas"} = props; // object destruc
@@ -12,7 +13,12 @@ export default function Greeting(props) {
     );
 }
 
-Greeting.defaultProps = {
+/* Greeting.defaultProps = {
     firstname: 'Ron',
     lastname: 'Con pasas',
+}; */
+
+Greeting.propTypes = {
+    firstname: PropTypes.string.isRequired, // indicando que es de tipo string y obligatorio
+    lastname: PropTypes.string.isRequired,
 }
